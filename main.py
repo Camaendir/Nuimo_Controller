96b2e6e26533c8b3baebe6e3a2a6c440898f90c5
Camaendir <dev@camaendir.de>
2021-06-23 14:43:31 +0200

    Added more matrices

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import nuimo
 import threading
-import paho.mqtt.client as mqtt
-import paho.mqtt.subscribe as subscribe
 import math
 from time import sleep
 
 
 VERBOSE = False
+WITHOUTMQTT = True
+
+
+if not WITHOUTMQTT:
+    import paho.mqtt.client as mqtt
+    import paho.mqtt.subscribe as subscribe
+
+
 play_matrix = nuimo.LedMatrix(
     "".join(
         [
             "         ",
+            "  *      ",
+            "  **     ",
             "  ***    ",
-            "  * **   ",
-            "  *  **  ",
-            "  *   ** ",
-            "  *  **  ",
-            "  * **   ",
+            "  *****  ",
             "  ***    ",
+            "  **     ",
+            "  *      ",
             "         "
         ]
     )
@@ -28,6 +34,59 @@ pause_matrix = nuimo.LedMatrix(
         [" "*9] + ["  ** **  " for _ in range(7)] + [" "*9]
     )
 )
+
+next_symbol =     "".join(
+        [
+            "         ",
+            "         ",
+            "  *  *   ",
+            "  ** *   ",
+            "  ****   ",
+            "  ** *   ",
+            "  *  *   ",
+            "         ",
+            "         "
+        ]
+    )
+
+next_matrix = nuimo.LedMatrix(
+    next_symbol
+)
+
+last_matrix = nuimo.LedMatrix(
+    next_symbol[::-1]
+)
+
+music_matrix = nuimo.LedMatrix(
+    "".join([
+        "  ***** *",
+        "  *****  ",
+        "  *   *  ",
+        "  *   *  ",
+        "  *   *  ",
+        " **  **  ",
+        "*** ***  ",
+        " *   *   ",
+        "         "
+    ])
+)
+
+light_matrix = nuimo.LedMatrix(
+    "".join([
+        "    *    ",
+        " *     * ",
+        "         ",
+        "    +    ",
+        "+  +*+  +",
+        "    +    ",
+        "         ",
+        " *     * ",
+        "    *    "
+    ])
+)
+
+matrix_list = (play_matrix, pause_matrix, next_matrix, last_matrix, music_matrix, light_matrix)
+
 
 def print_ln(*args):
     if VERBOSE:
@@ -48,6 +107,8 @@ def update_matrix_status(status):
     else:
         controller.display_matrix(pause_matrix, interval=5, fading=True)
 
+def update_matrix_test(position):
+    controller.display_matrix(matrix_list[position], interval=10, fading=False)
 
 def on_message(client, userdata, message):
     print_ln("mqtt message recieved", message)
@@ -64,15 +125,17 @@ def on_connect(client, userdata, flags, rc):
 class MQTTListener(nuimo.ControllerListener):
 
     def __init__(self):
-        self.client = mqtt.Client("Nuimo")
-        self.client.connect("localhost")
         self.buffer = []
+        self.matrix_position = 0
         self.running = False
         self.thread = threading.Thread(target=self.send_average)
-        self.client.on_message = on_message
-        self.client.on_disconnect = reconnect_client
-        self.client.on_connect = on_connect
-        self.client.loop_start()
+        if WITHOUTMQTT:
+            self.client = mqtt.Client("Nuimo")
+            self.client.connect("localhost")
+            self.client.on_message = on_message
+            self.client.on_disconnect = reconnect_client
+            self.client.on_connect = on_connect
+            self.client.loop_start()
 
     def publish_volume_increase(self, volume):
         self.client.publish("spotify/volume/increase", str(volume))
@@ -100,6 +163,13 @@ class MQTTListener(nuimo.ControllerListener):
                 self.thread.start()
         elif event.gesture == nuimo.Gesture.BUTTON_PRESS:
             self.send_play_pause()
+        elif event.gesture == nuimo.Gesture.SWIPE_LEFT:
+            self.matrix_position += 1 % len(matrix_list)
+            update_matrix_test(self.matrix_position)
+        elif event.gesture == nuimo.Gesture.SWIPE_RIGHT:
+            self.matrix_position -= 1 % len(matrix_list)
+            update_matrix_test(self.matrix_position)
+
 
     def started_connecting(self):
         print_ln("started connecting")
